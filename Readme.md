a00d93f009df3764802338791e3b9b34f0b8f37e
Marcin Bratek <github@bratekmarcin.eu>
2024-02-23 21:21:20 +0100

    Initial version 0.1.0

---
+++ Readme.md
@@ -0,0 +1,25 @@
+FoundryVTT module for the Imperium Maledictum system which handles Resolve in combat and helps tracking defeated enemies each round.
+
+# Features
+
+## Count how many enemies are defeated
+
+After each round, module counts how many Troops, Elites and Leaders were defeated last round and sends summary to chat or notification. Why? To remember to add Superiority, of course! (p. 197 Imperium Maledictum Core Book)
+
+## List Troop NPCs which are below Superiority treshold
+
+List clickable tokens in chat message which belongs to Troop NPC whose Resolve is equal to or below the Superiority (p. 197 Imperium Maledictum Core Book). 
+
+# Examples
+
+Chat Message listing defeated enemies and lists Troop NPC with Resolve below Superiority treshold.
+
+![](metadata/example_1.png)
+
+Instead of Chat Message, notification is also an option.
+
+![](metadata/example_2.png)
+
+Settings can be adjusted to just count specific NPCs type, and more.
+
+![](metadata/example_3.png)
---
+++ resolver.js
@@ -0,0 +1,144 @@
+Hooks.on("combatRound", (combat) => 
+{
+    // Here instead combatStart, because Foundry can be reloaded during fight
+    combat.settings = combat.settings ?? {}; // it was not there on fresh world?
+    combat.settings.previousDefeatedTroops = combat.settings.previousDefeatedTroops ?? 0;
+    combat.settings.previousDefeatedElites = combat.settings.previousDefeatedElites ?? 0;
+    combat.settings.previousDefeatedLeaders = combat.settings.previousDefeatedLeaders ?? 0;
+
+    let currentDefeated = combat.combatants.filter( a => a.isDefeated && a.actor.type === 'npc' );
+
+    const countElites = game.settings.get('impmal-combat-resolve','countElites');
+    const countLeaders = game.settings.get('impmal-combat-resolve','countLeaders');
+    const countTroops = game.settings.get('impmal-combat-resolve','countTroops');
+
+    let diffTroops = 0;
+    let diffElites = 0;
+    let diffLeaders = 0;    
+
+    if (countTroops) {
+        const defeatedTrops = currentDefeated.map( a => a.actor.system.role === 'troop' ).filter( a => a == true ).length;
+        diffTroops = defeatedTrops - combat.settings.previousDefeatedTroops;
+        if (diffTroops < 0) {
+            diffTroops = 0;
+        }
+        combat.settings.previousDefeatedTroops = defeatedTrops;
+    }
+    if (countElites) {
+        const defeatedElites = currentDefeated.map( a => a.actor.system.role === 'elite' ).filter( a => a == true ).length;
+        diffElites = defeatedElites - combat.settings.previousDefeatedElites;
+        if (diffElites < 0) {
+            diffElites = 0;
+        }
+        combat.settings.previousDefeatedElites = defeatedElites;
+    }
+    if (countLeaders) {
+        const defeatedLeaders = currentDefeated.map( a => a.actor.system.role === 'leader' ).filter( a => a == true ).length;
+        diffLeaders = defeatedLeaders - combat.settings.previousDefeatedLeaders;
+        if (diffLeaders < 0) {
+            diffLeaders = 0;
+        }
+        combat.settings.previousDefeatedLeaders = defeatedLeaders;
+    }
+
+    if (diffTroops > 0 || diffElites > 0 || diffLeaders > 0) {
+        if (game.settings.get('impmal-combat-resolve','sendToChat')) {
+            let restNpcs;
+            if (game.settings.get('impmal-combat-resolve','showNPCBelowSuperiority')) {
+                restNpcs = combat.combatants.filter( a => !a.isDefeated && a.actor.type === 'npc' && a.actor.system.role === 'troop' && a.actor.system.combat.resolve <= game.settings.get("impmal", "superiority") ).map( a => a.token );
+                console.log(restNpcs);
+            }
+            ResolveMessage.postToChat( diffTroops, diffElites, diffLeaders, restNpcs ) 
+        } else {
+            let message = game.i18n.localize('IMPMAL-COMBAT-RESOLVE.MESSAGES.inLastRound');
+            if (diffTroops > 0) {
+                message += " " + diffTroops + " " + game.i18n.localize('IMPMAL-COMBAT-RESOLVE.MESSAGES.troopsDefeated');
+            }
+            if (diffElites > 0) {
+                message += " " + diffElites + " " + game.i18n.localize('IMPMAL-COMBAT-RESOLVE.MESSAGES.elitesDefeated');
+            }
+            if (diffLeaders > 0) {
+                message += " " + diffLeaders + " " + game.i18n.localize('IMPMAL-COMBAT-RESOLVE.MESSAGES.leadersDefeated');
+            }
+            ui.notifications.info(message);
+        }
+    }
+});
+
+Hooks.on("init", () => {
+    game.settings.register('impmal-combat-resolve', 'sendToChat', {
+        name: 'IMPMAL-COMBAT-RESOLVE.SETTINGS.sentToChat',
+        hint: 'IMPMAL-COMBAT-RESOLVE.SETTINGS.sentToChatHint',
+        scope: 'world',     // "world" = sync to db, "client" = local storage 
+        config: true,       // false if you dont want it to show in module config
+        type: Boolean,       // Number, Boolean, String,  
+        default: true
+    });
+
+    game.settings.register('impmal-combat-resolve', 'showNPCBelowSuperiority', {
+        name: 'IMPMAL-COMBAT-RESOLVE.SETTINGS.showNPCBelowSuperiority',
+        hint: 'IMPMAL-COMBAT-RESOLVE.SETTINGS.showNPCBelowSuperiorityHint',
+        scope: 'world',     // "world" = sync to db, "client" = local storage 
+        config: true,       // false if you dont want it to show in module config
+        type: Boolean,       // Number, Boolean, String,  
+        default: true
+    });
+
+    game.settings.register('impmal-combat-resolve', 'chatVisibility', {
+        name: 'IMPMAL-COMBAT-RESOLVE.SETTINGS.chatVisibility',
+        hint: 'IMPMAL-COMBAT-RESOLVE.SETTINGS.chatVisibilityHint',
+        scope: 'world',     // "world" = sync to db, "client" = local storage 
+        config: true,       // false if you dont want it to show in module config
+        type: String,       // Number, Boolean, String,  
+        default: true,
+        choices: {
+            private: "IMPMAL-COMBAT-RESOLVE.SETTINGS.chatVisibilityPrivate",
+            public: "IMPMAL-COMBAT-RESOLVE.SETTINGS.chatVisibilityPublic",
+        },
+    });
+
+    game.settings.register('impmal-combat-resolve', 'countTroops', {
+        name: 'IMPMAL-COMBAT-RESOLVE.SETTINGS.countTroops',
+        scope: 'world',     // "world" = sync to db, "client" = local storage 
+        config: true,       // false if you dont want it to show in module config
+        type: Boolean,       // Number, Boolean, String,  
+        default: true
+    });
+
+    game.settings.register('impmal-combat-resolve', 'countElites', {
+        name: 'IMPMAL-COMBAT-RESOLVE.SETTINGS.countElites',
+        scope: 'world',     // "world" = sync to db, "client" = local storage 
+        config: true,       // false if you dont want it to show in module config
+        type: Boolean,       // Number, Boolean, String,  
+        default: true
+    });
+
+    game.settings.register('impmal-combat-resolve', 'countLeaders', {
+        name: 'IMPMAL-COMBAT-RESOLVE.SETTINGS.countLeaders',
+        scope: 'world',     // "world" = sync to db, "client" = local storage 
+        config: true,       // false if you dont want it to show in module config
+        type: Boolean,       // Number, Boolean, String,  
+        default: true
+    });
+
+});
+
+class ResolveMessage {
+    static async postToChat(diffTroops, diffElites, diffLeaders, npcs) {
+        const template_file = "modules/impmal-combat-resolve/templates/chat_message.hbs";
+        const template_data = { 
+            diffTroops: diffTroops,
+            diffElites: diffElites,
+            diffLeaders: diffLeaders,
+            npcs: npcs
+        };
+        const rendered_html = await renderTemplate(template_file, template_data);
+
+        let params = { content: rendered_html };
+        const chatVisibility = game.settings.get('impmal-combat-resolve','chatVisibility');
+        if (chatVisibility === 'private') {
+            params.whisper = ChatMessage.getWhisperRecipients('GM')
+        }
+        ChatMessage.create(params);
+    }
+}
